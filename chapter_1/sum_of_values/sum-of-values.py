ce3a695985adfa97e4a8b2e34c3d2126d16a8217
irclemens <iclemens@andrew.cmu.edu>
2026-09-24 06:27:13 -0400

    Complete SumOfValues in python

---
+++ chapter_1/sum_of_values/sum-of-values.py
@@ -0,0 +1,19 @@
+import sys
+# Please do not remove package declarations because these are used by the autograder. If you need additional packages, then you may declare them above.
+
+# Insert your sum_of_values() function here.
+def sum_of_values(sample: dict) -> int:
+    """
+    sum_of_values finds the sum of all the integer values in the 
+    key of the dictionary.
+
+    Parameters:
+    - sample1 (dict): The sample or frequency table.
+
+    Returns:
+    - int: The sum of the keys in the given sample1.
+    """
+    x = 0
+    for val in sample.values():
+        x += val
+    return x
